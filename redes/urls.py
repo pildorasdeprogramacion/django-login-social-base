4e3a6c61c9594e0e929df7226b4c9d5c4627b1a9
edward <tu_email@example.com>
2024-03-04 14:28:30 -0500

    adding general urls django-social

--- redes/urls.py
+++ redes/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('social-auth/',include('social_django.urls', namespace='social')),
 ]
